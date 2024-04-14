457f333779efa70ce7c484a943da2fb2dab82559
youjae <youjae0803@gmail.com>
2024-04-14 15:21:29 +0900

    init: project setting

---
+++ src/main/java/com/example/backendraspberrypiserver/BackendRaspberrypiServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendraspberrypiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRaspberrypiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRaspberrypiServerApplication.class, args);
+	}
+
+}
