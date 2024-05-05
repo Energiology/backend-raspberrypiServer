90f22a96eb287962aa129410edc6af048e3cdbc8
j2noo <wlsdndml213@gmail.com>
2024-05-05 14:27:43 +0900

    fix :  오타 수정

--- src/main/java/com/example/backendraspberrypiserver/TestController.java
+++ src/main/java/com/example/backendraspberrypiserver/TestController.java
@@ -5,15 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("api/health")
+@Slf4j
 public class TestController {
-    @RestController
-    @RequestMapping("api/health")
-    @Slf4j
-    public class HealthTestController {
-        @GetMapping("")
-        public String apiHealthTest() {
-            return "rasberry !";
-        }
+    @GetMapping("")
+    public String apiHealthTest() {
+        return "rasberry !";
 
     }
 }
